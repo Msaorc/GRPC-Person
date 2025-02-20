3e4071ea3adac503edf2fe5657a587fc1ad523cb
marcos <msaorc@hotmail.com>
2025-02-20 18:13:06 -0300

    Criação do service de Profession

---
+++ internal/service/profession.go
@@ -0,0 +1,33 @@
+package service
+
+import (
+	"context"
+
+	"github.com/Msaorc/GRPC-Person/internal/database"
+	"github.com/Msaorc/GRPC-Person/internal/pb"
+)
+
+type ProfessionService struct {
+	pb.UnimplementedPersonServiceServer
+	ProfessionDB database.Profession
+}
+
+func NewProfessionService(professionDB database.Profession) *ProfessionService {
+	return &ProfessionService{
+		ProfessionDB: professionDB,
+	}
+}
+
+func (pf *ProfessionService) CreateProfession(ctx context.Context, in *pb.CreateProfessionRequest) (*pb.ProfessionResponse, error) {
+	profession, err := pf.ProfessionDB.Create(in.Description)
+	if err != nil {
+		return nil, err
+	}
+
+	professionResponse := &pb.Profession{
+		Id:          profession.ID,
+		Description: profession.Description,
+	}
+
+	return &pb.ProfessionResponse{Profession: professionResponse}, nil
+}
